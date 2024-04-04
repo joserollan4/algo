0a8829f44798122508c7b12f55f54684334aa477
Jose Mari Paolo Rollan <149309504+joserollan4@users.noreply.github.com>
2024-04-04 14:20:25 +0800

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,24 @@
+#include "bits/stdc++.h"
+using namespace std;
+using ll = long long;
+
+// Problem ___
+void sol(int Case) {
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    //freopen("input.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+    
+    int t;
+    cin >> t;
+    for (int i=1; i<=t; i++) {
+        sol(i);
+        if (i != t) cout << '\n';
+    }
+    return 0;   
+}
