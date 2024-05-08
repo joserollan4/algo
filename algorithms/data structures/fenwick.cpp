ee77b89d497b85468dc517df5587b13ed22f6311
Jose Mari Paolo Rollan <149309504+joserollan4@users.noreply.github.com>
2024-05-08 18:43:55 +0800

    Update fenwick.cpp

--- algorithms/data structures/fenwick.cpp
+++ algorithms/data structures/fenwick.cpp
@@ -1,27 +1,24 @@
 struct Fenwick {
-    int n; // size of Fenwick tree
-    vector<ll> bit; // Fenwick tree array
+    int n;
+    vector<ll> bit;
     
-    void init(int m) { // initialize array
+    void init(int m) {
         n = m;
-        for (int i=0; i<=n; i++) {
-            bit.push_back(0);
-        }
+        for (int i=0; i<=n; i++) bit.push_back(0);
     }   
-    
-    void add(int idx, ll val) { // does a[i] += x
-        for (int i=idx; i<=n; i+=i&-i) {
-            bit[i] += val;
+    void add(int idx, ll val) {
+        for (idx++; idx<=n; idx+=idx&-idx) {
+            bit[idx] += val;
         }   
-    }      
-    void range_add(int l, int r, ll val) { // does a[i] += x for l <= i <= r
+    }   
+    void range_add(int l, int r, ll val) {
         add(l,val);
         add(r+1,-val);
     }   
-    ll get(int idx) { // returns value of a[i]
+    ll get(int idx) {
         ll res = 0;
-        for (int i=idx; i>0; i-=i&-i) { 
-            res += bit[i];
+        for (idx++; idx>0; idx-=idx&-idx) {
+            res += bit[idx];
         }   
         return res;
     }   
