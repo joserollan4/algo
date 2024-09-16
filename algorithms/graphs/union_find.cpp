ac86e2e0a46c671307d1ef8ef99b195615c2320d
Jose Mari Paolo Rollan <149309504+joserollan4@users.noreply.github.com>
2024-09-16 11:48:58 +0800

    Create union find (basic).cpp

---
+++ algorithms/graphs/union_find.cpp
@@ -0,0 +1,23 @@
+// basic union find
+// find_set returns parent node of node v in O(lg n)
+// unite "unites" (adds an edge from) a to b in O(lg n)
+vector<int> parent;
+
+void make_set(int v) {
+    parent[v] = v;
+}   
+
+int find_set(int v) {
+    if (v == parent[v]) {
+        return v;
+    }   
+    return parent[v] = find_set(parent[v]);
+}   
+
+void unite(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        parent[b] = a;
+    }   
+}   
