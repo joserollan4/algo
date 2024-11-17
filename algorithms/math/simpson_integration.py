9e6687bfc35d8e01d4d0460c1024cd95d8f3d289
Jose Mari Paolo Rollan <149309504+joserollan4@users.noreply.github.com>
2024-11-18 01:49:33 +0800

    Update simpson_integration.py

--- algorithms/math/simpson_integration.py
+++ algorithms/math/simpson_integration.py
@@ -1,12 +1,12 @@
 def simpsons(A, B, *args): # integral of f(x)dx from A to B
     N = 100000
     h = (B - A) / N
-    s = f(B,*args) + f(A,*args) # f is some function
+    ans = f(B,*args) + f(A,*args) # f is some function
     for i in range(1,N):
         x = A + h * i
         if i % 2 == 1:
-            s += (f(x,a,b) * 4)
+            ans += (f(x,*args) * 4)
         else:
-            s += (f(x,a,b) * 2)
-    s *= (h/3)
+            s += (f(x,*args) * 2)
+    ans *= (h/3)
     return s
