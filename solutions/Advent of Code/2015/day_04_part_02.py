8d8b4848b71ed2dbb9edc441ac13c0615d7f1011
Jose Mari Paolo Rollan <149309504+joserollan4@users.noreply.github.com>
2025-01-08 11:07:50 +0800

    Create day_04_part_02.py

---
+++ solutions/Advent of Code/2015/day_04_part_02.py
@@ -0,0 +1,11 @@
+import hashlib
+# adapted from https://www.geeksforgeeks.org/md5-hash-python/
+def md5(s):
+    return hashlib.md5(s.encode()).hexdigest()
+
+ans = 1
+while True:
+    if md5("iwrupvqb"+str(ans))[:6] == '000000':
+        print(ans)
+        exit()
+    ans += 1
